f885eaa343c08e6309e9b02f3765eaee67f1182f
nattapad <42264165+nattapad10@users.noreply.github.com>
2020-09-30 15:09:29 +0700

    Update ch1.js

--- ch1.js
+++ ch1.js
@@ -1,1 +1,6 @@
-console.log('Name : Nattapad Mueankid');
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Name : Nattapad Mueankid');
+}).listen(8080);
