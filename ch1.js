ba422cb2f32dc2e8d998d94b0648389b96844b2a
nattapad <42264165+nattapad10@users.noreply.github.com>
2020-09-30 15:33:42 +0700

    Update ch1.js

--- ch1.js
+++ ch1.js
@@ -3,4 +3,6 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Name : Nattapad Mueankid');
+  res.end('nickname : An');
+  res.end('Student code : 60030029');
 }).listen(3007);
